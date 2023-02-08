f1dbfb34ce713b8ef6fc24093f8617b64edf6901
jlabrada <jlabrada@yahoo.com>
2023-02-08 13:00:26 -0300

    Adding counter store

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { useCounterStore } from "./stores/counter"
+  const store = useCounterStore(); //initialize the store
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Counter App</h1>
+  <p>Count is {{ store.count }}</p>
+  <p>Double count is {{ store.doubleCount }}</p>
+  <button @click="store.increment(1)">Add</button>
+  <button @click="store.decrement(1)">Subtract</button>
+  <button @click="store.reset">Reset</button>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
